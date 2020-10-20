12afe86b776df342bc71c0404e9816a2c57c8236
Amph <amprubio@ucm.es>
2020-10-20 16:24:44 +0200

    clase date el campus

--- Entregas Laboratorio/Sesion3Lab/Sesion3Lab/Date.h
+++ Entregas Laboratorio/Sesion3Lab/Sesion3Lab/Date.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
 class Date
 {
+public:
+	Date() : day(0), month(0), year(0) {};
+	Date(int d, int m, int y);
+	Date(const Date& date);
+	void operator++();
+	void operator+=(int n);
+	bool operator<(const Date& date) const;
+	int diff(const Date& date) const;
+	void print() const;
+	friend istream& operator>>(istream& in, Date& d);
+	friend ostream& operator<<(ostream& in, const Date& d);
+private:
+	int day;
+	int month;
+	int year;
+
+	int daysInMonth(int m) const;
+	int calculateValue() const;
+	bool correctDate() const;
 };
 
